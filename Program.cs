bef57b9b10f9b8650f00ae0863c4ac5881397394
Tiago Rodrigues <isatiago098@gmail.com>
2021-09-19 21:11:30 -0300

    resoluçao exerciçio medidas

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+       {
+           double metrosDigitado, metrosParaCm, metrosParaKm;
+           Console.Write("entre com a medida em metros (m)");
+           metrosDigitado =Convert.ToDouble(Console.ReadLine());
+
+           Console.WriteLine("---Equivalençia---");
+
+           metrosParaCm = metrosDigitado/100;
+
+           Console.WriteLine($"{metrosParaCm} Cm");
+
+           Console.WriteLine($"{metrosDigitado} m");
+
+           metrosParaKm = metrosDigitado/1000;
+
+            Console.WriteLine($"{metrosParaKm} Km");
+
+ }         
+       }
+}
+    
